8cf84ec145a92c3c2fb20d5980f6cab6ad8458cf
Denis Wells <deniswells59@gmail.com>
2017-08-29 21:27:27 -0700

    gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('yo');
+(function() {
+
+  function galleryPrevSize() {
+    let chalk = document.getElementsByClassName('chalk')[3];
+    let prevs = document.getElementsByClassName('gallery-prev');
+    let fullHeight = chalk.offsetHeight || chalk.clientHeight;
+    for(let key in prevs) {
+      if(prevs.hasOwnProperty(key)) {
+        let p = prevs[key];
+        p.style.height = `${fullHeight / 2}px`;
+      }
+    }
+  }
+
+  function initResponsive() {
+    galleryPrevSize();
+
+    window.addEventListener('resize', galleryPrevSize, true);
+  }
+
+  initResponsive();
+})();
